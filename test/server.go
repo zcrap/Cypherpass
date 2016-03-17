e0903c29b2bef6eec519a46734d710f98f87eef7
Zach Collier <zamicol@gmail.com>
2016-03-17 00:22:18 -0600

    test server work

--- test/server.go
+++ test/server.go
@@ -18,15 +18,25 @@ func init() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Received Request: " + r.URL.Path)
+	//http.ServeFile(w, r, r.URL.Path[1:])
+	http.FileServer(http.Dir("login"))
 
 	body, _ := ioutil.ReadAll(r.Body)
 	fmt.Println(body)
 
 }
 
-func main() {
-	fmt.Println("listening on port: " + *port)
+//func main() {
+//	fmt.Println("listening on port: " + *port)
 
-	http.HandleFunc("/", handler)
+//	http.HandleFunc("/", handler)
+//	http.ListenAndServe(*port, nil)
+//}
+
+func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+
+	fmt.Println("Listening...")
 	http.ListenAndServe(*port, nil)
 }
